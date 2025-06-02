e5531b45ba0427be015fa78a3102a4f8fab4e6c4
hasharas <hasharamsankalpam@gmail.com>
2025-06-03 00:23:39 +0530

    Create:apis create get trend movie and details

---
+++ src/lib/tmdb.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getTrendingMovies = async () => {
+      const res = await axios.get(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+      return res.data.results;
+};
+
+export const getMovieDetails = async (id: string) => {
+      const res = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+      return res.data;
+};
