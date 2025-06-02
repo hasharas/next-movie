b58b694acaf99d92f8f95ccfabd85c1134b9fda8
hasharas <hasharamsankalpam@gmail.com>
2025-06-03 00:53:13 +0530

    Style:align item nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,19 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-black text-white p-4">
+    <nav className="bg-black flex justify-between items-center text-white p-4">
       <Link href="/" className="text-xl font-bold">🎥 Movie Explorer</Link>
+      <ul className="flex space-x-4 items-center text-sm text-gray-300">
+        <li>
+          <Link href="/" className="hover:underline">Home</Link>
+        </li>
+        <li>
+          <Link href="/about" className="hover:underline">About</Link>
+        </li>
+        <li>
+          <Link href="/contact" className="hover:underline">Contact</Link>
+        </li>     
+      </ul>
     </nav>
   );
 }
